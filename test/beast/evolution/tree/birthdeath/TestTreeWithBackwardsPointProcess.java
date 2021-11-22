2dcb933a520cbe404453c2f6fb6c09ca5a862f85
Alex Zarebski <aezarebski@gmail.com>
2021-11-22 17:46:55 +0000

    include a test that things will work without a rho sample

--- test/beast/evolution/tree/birthdeath/TestTreeWithBackwardsPointProcess.java
+++ test/beast/evolution/tree/birthdeath/TestTreeWithBackwardsPointProcess.java
@@ -1,18 +1,117 @@
 package beast.evolution.tree.birthdeath;
 
 import beast.core.parameter.RealParameter;
+import beast.evolution.alignment.Alignment;
+import beast.evolution.alignment.Sequence;
+import beast.evolution.alignment.TaxonSet;
+import beast.evolution.tree.RandomTree;
+import beast.evolution.tree.TraitSet;
 import beast.evolution.tree.Tree;
+import beast.evolution.tree.coalescent.ConstantPopulation;
+import beast.math.distributions.MRCAPrior;
 import beast.util.TreeParser;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.BiPredicate;
 
 public class TestTreeWithBackwardsPointProcess {
 
     private final BiPredicate<Double, Double> approxEqual = (x, y) -> Math.abs(x - y) < 1e-5;
+
+    @Test
+    public void canHandleEstimatedTree() {
+
+        ConstantPopulation myPopModel = new ConstantPopulation();
+        myPopModel.initByName("popSize", "1.0");
+
+        List<Sequence> mySeqs = new ArrayList<>();
+        mySeqs.add(new Sequence("t0_8.0", "?"));
+        mySeqs.add(new Sequence("t1_9.5", "?"));
+        Alignment myTaxa = new Alignment(mySeqs, "nucleotide");
+        TaxonSet myTaxonSet = new TaxonSet(myTaxa);
+
+        // TODO put a constraint on this to ensure that the origin occurs before the first occurrence!
+        TraitSet myTraitSet = new TraitSet();
+        myTraitSet.initByName(
+                "traitname", "date",
+                "value", "t0_8.0=8.0,t1_9.5=9.5",
+                "taxa", myTaxonSet
+        );
+
+
+
+        RandomTree myTree;
+        myTree = new RandomTree();
+        myTree.initByName(
+                "taxonset", myTaxonSet,
+                "trait", myTraitSet,
+                "populationModel", myPopModel);
+
+        double rootLengthDouble = 1.0;
+        RealParameter rootLength = new RealParameter("1.0");
+
+
+        // This loop is included to ensure that we get an origin that occurs before the first occurrence event.
+        double mRCAHeight;
+        mRCAHeight = myTree.getRoot().getHeight();
+        while (mRCAHeight + rootLengthDouble < 4.5) {
+            myTree = new RandomTree();
+            myTree.initByName(
+                    "taxonset", myTaxonSet,
+                    "trait", myTraitSet,
+                    "populationModel", myPopModel);
+            mRCAHeight = myTree.getRoot().getHeight();
+        }
+        assertTrue(mRCAHeight + rootLengthDouble > 4.5);
+
+
+        BackwardsPointProcess points = new BackwardsPointProcess();
+        points.initByName("value", "4.5 0.5");
+
+        TreeWithBackwardsPointProcess tpp = new TreeWithBackwardsPointProcess(
+                rootLength,
+                myTree,
+                points,
+                null);
+
+        // There are two occurrences, two leaves and one internal nodes. The "present" is the time of the last tip.
+        assertEquals(5, tpp.getIntervalCount());
+
+        double hiddenDelay;
+        if (Objects.equals(tpp.getIntervalType(0).toString(), "birth")) {
+            assertEquals(tpp.getIntervalType(0).toString(), "birth");
+            assertTrue(approxEqual.test(tpp.getIntervalDuration(0), 1.0));
+            assertEquals(tpp.getIntervalType(1).toString(), "occurrence");
+            assertTrue(tpp.getIntervalDuration(1) > 0.0);
+            hiddenDelay = tpp.getIntervalDuration(1) + 1.0;
+        } else {
+            assertEquals(tpp.getIntervalType(0).toString(), "occurrence");
+            assertTrue(tpp.getIntervalDuration(0) > 0);
+            assertEquals(tpp.getIntervalType(1).toString(), "birth");
+            assertTrue(tpp.getIntervalDuration(1) < 1.0);
+            hiddenDelay = tpp.getIntervalDuration(0) + tpp.getIntervalDuration(1);
+        }
+
+        assertEquals(tpp.getIntervalType(2).toString(), "sample");
+        assertTrue(approxEqual.test(
+                hiddenDelay + tpp.getIntervalDuration(2),
+                mRCAHeight + rootLengthDouble - 1.5));
+
+
+        assertEquals(tpp.getIntervalType(3).toString(), "occurrence");
+        assertTrue(approxEqual.test(tpp.getIntervalDuration(3), 1.0));
+
+        assertEquals(tpp.getIntervalType(4).toString(), "sample");
+        assertTrue(approxEqual.test(tpp.getIntervalDuration(4), 0.5));
+
+    }
 
     @Test
     public void canRecoverCatastrophe() {
