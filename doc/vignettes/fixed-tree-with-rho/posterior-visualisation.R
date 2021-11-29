8fd2663f4ebf08686647c95c98e8a72f3a0d767b
Alexander E. Zarebski <36244258+aezarebski@users.noreply.github.com>
2021-11-29 16:06:04 +0000

    Est tree no rho again (#12)
    * update ape-sim script
    
    * update example to use beauti generated xml
    
    * update template and example to estimate omega
    
    * estimate more stuff
    
    * include a bdsky example as a point of comparison

--- doc/vignettes/fixed-tree-with-rho/posterior-visualisation.R
+++ doc/vignettes/fixed-tree-with-rho/posterior-visualisation.R
@@ -2,48 +2,53 @@ library(ggplot2)
 library(cowplot)
 library(jsonlite)
 
-posterior_samples <- read.csv("out/fixed-tree-posterior-samples.log",
-                              sep = "\t")
+posterior_samples <- read.csv("out/fixed-tree-with-rho.log",
+                              sep = "\t", comment.char = "#")
 true_parameters <- as.data.frame(read_json("my-params.json"))
-true_final_prev <- read_json("out/ape-sim-final-prevalence.json",
+true_parameters$rNaught <- true_parameters$birthRate / (true_parameters$deathRate + true_parameters$samplingRate + true_parameters$occurrenceRate)
+true_parameters$prevalence <- read_json("out/ape-sim-final-prevalence.json",
                              simplifyVector = TRUE)
 
-
-prev_post_samples <- rnbinom(
-  n = 10 * nrow(posterior_samples),
-  size = exp(posterior_samples$myTimTam.prevalence.lnR),
-  prob = 1 - exp(posterior_samples$myTimTam.prevalence.lnP))
+posterior_samples$prevalence <- rnbinom(
+  n = nrow(posterior_samples),
+  size = exp(posterior_samples$TimTam.prevalence.lnR),
+  prob = 1 - exp(posterior_samples$TimTam.prevalence.lnP))
+posterior_samples$rNaught <- posterior_samples$birthRate / (posterior_samples$deathRate + posterior_samples$samplingRate + posterior_samples$occurrenceRate)
 
 g1 <- ggplot() +
   geom_hex(data = posterior_samples,
-           mapping = aes(x = myLambda, y = myPsi),
+           mapping = aes(x = rNaught, y = prevalence),
            bins = 20) +
   geom_point(data = true_parameters,
-             mapping = aes(x = birthRate, y = samplingRate),
+             mapping = aes(x = rNaught, y = prevalence),
              size = 4,
              colour = "red") +
-  labs(x = "Birth rate", y = "Sampling rate") +
+  labs(x = "R-naught", y = "Prevalence") +
   scale_fill_continuous(low = "white", high = "black") +
   theme_classic() +
   theme(legend.position = "none")
 
-g2 <- ggplot(mapping = aes(x = x, y = ..density..)) +
-  geom_histogram(data = data.frame(x = prev_post_samples),
-                 bins = 20) +
-  geom_vline(xintercept = true_final_prev,
-             colour = "red",
-             size = 2,
-             linetype = "dashed") +
-  labs(x = "Final prevalence", y = "Posterior distribution") +
-  theme_classic()
-
-g <- plot_grid(g1, g2)
-
-
 ggsave(
   filename = "./out/posterior-plot.png",
-  plot = g,
+  plot = g1,
   height = 10.5,
-  width = 22.5,
+  width = 10.5,
   units = "cm"
 )
+
+bdsky_posterior_samples <- read.csv("out/fixed-tree-with-rho-again.log",
+                                    sep = "\t", comment.char = "#")
+plot_df <- data.frame(model = rep(c("timtam", "bdsky"), each = nrow(posterior_samples)),
+                      rNaught = c(posterior_samples$rNaught, bdsky_posterior_samples$reproductiveNumber_BDSKY_Serial))
+
+g2 <- ggplot() +
+  geom_histogram(data = plot_df, mapping = aes(x = rNaught)) +
+  geom_vline(xintercept = true_parameters$rNaught, linetype = "dashed") +
+  labs(x = "R-naught", y = "Posterior samples") +
+  facet_wrap(~model) +
+  theme_classic()
+
+ggsave(filename = "./out/r-naught-comparison.png",
+       plot = g2,
+       height = 14.8, width = 21.0,
+       units = "cm")
