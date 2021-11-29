a56054a9a1e56a47e20afce99b61f52228a41f06
Alex Zarebski <aezarebski@gmail.com>
2021-11-29 15:27:14 +0000

    estimate more stuff

--- doc/vignettes/fixed-tree-with-rho/posterior-visualisation.R
+++ doc/vignettes/fixed-tree-with-rho/posterior-visualisation.R
@@ -5,45 +5,32 @@ library(jsonlite)
 posterior_samples <- read.csv("out/fixed-tree-with-rho.log",
                               sep = "\t", comment.char = "#")
 true_parameters <- as.data.frame(read_json("my-params.json"))
-true_final_prev <- read_json("out/ape-sim-final-prevalence.json",
+true_parameters$rNaught <- true_parameters$birthRate / (true_parameters$deathRate + true_parameters$samplingRate + true_parameters$occurrenceRate)
+true_parameters$prevalence <- read_json("out/ape-sim-final-prevalence.json",
                              simplifyVector = TRUE)
 
-
-prev_post_samples <- rnbinom(
-  n = 10 * nrow(posterior_samples),
+posterior_samples$prevalence <- rnbinom(
+  n = nrow(posterior_samples),
   size = exp(posterior_samples$TimTam.prevalence.lnR),
   prob = 1 - exp(posterior_samples$TimTam.prevalence.lnP))
 
-g1 <- ggplot() +
+g <- ggplot() +
   geom_hex(data = posterior_samples,
-           mapping = aes(x = birthRate, y = samplingRate),
+           mapping = aes(x = birthRate / (deathRate + samplingRate + occurrenceRate), y = prevalence),
            bins = 20) +
   geom_point(data = true_parameters,
-             mapping = aes(x = birthRate, y = samplingRate),
+             mapping = aes(x = rNaught, y = prevalence),
              size = 4,
              colour = "red") +
-  labs(x = "Birth rate", y = "Sampling rate") +
+  labs(x = "R-naught", y = "Prevalence") +
   scale_fill_continuous(low = "white", high = "black") +
   theme_classic() +
   theme(legend.position = "none")
-
-g2 <- ggplot(mapping = aes(x = x, y = ..density..)) +
-  geom_histogram(data = data.frame(x = prev_post_samples),
-                 bins = 20) +
-  geom_vline(xintercept = true_final_prev,
-             colour = "red",
-             size = 2,
-             linetype = "dashed") +
-  labs(x = "Final prevalence", y = "Posterior distribution") +
-  theme_classic()
-
-g <- plot_grid(g1, g2)
-
 
 ggsave(
   filename = "./out/posterior-plot.png",
   plot = g,
   height = 10.5,
-  width = 22.5,
+  width = 10.5,
   units = "cm"
 )
