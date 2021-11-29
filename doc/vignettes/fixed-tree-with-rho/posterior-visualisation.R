85348b3f700e91ab7ae0f93d3e6a5abed7003c82
Alex Zarebski <aezarebski@gmail.com>
2021-11-29 14:12:31 +0000

    update example to use beauti generated xml

--- doc/vignettes/fixed-tree-with-rho/posterior-visualisation.R
+++ doc/vignettes/fixed-tree-with-rho/posterior-visualisation.R
@@ -2,8 +2,8 @@ library(ggplot2)
 library(cowplot)
 library(jsonlite)
 
-posterior_samples <- read.csv("out/fixed-tree-posterior-samples.log",
-                              sep = "\t")
+posterior_samples <- read.csv("out/fixed-tree-with-rho.log",
+                              sep = "\t", comment.char = "#")
 true_parameters <- as.data.frame(read_json("my-params.json"))
 true_final_prev <- read_json("out/ape-sim-final-prevalence.json",
                              simplifyVector = TRUE)
@@ -11,12 +11,12 @@ true_final_prev <- read_json("out/ape-sim-final-prevalence.json",
 
 prev_post_samples <- rnbinom(
   n = 10 * nrow(posterior_samples),
-  size = exp(posterior_samples$myTimTam.prevalence.lnR),
-  prob = 1 - exp(posterior_samples$myTimTam.prevalence.lnP))
+  size = exp(posterior_samples$TimTam.prevalence.lnR),
+  prob = 1 - exp(posterior_samples$TimTam.prevalence.lnP))
 
 g1 <- ggplot() +
   geom_hex(data = posterior_samples,
-           mapping = aes(x = myLambda, y = myPsi),
+           mapping = aes(x = birthRate, y = samplingRate),
            bins = 20) +
   geom_point(data = true_parameters,
              mapping = aes(x = birthRate, y = samplingRate),
